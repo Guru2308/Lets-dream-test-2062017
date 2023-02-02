0e13bf6972fcac9c15a70471e98965701724e244
Guru2308 <chatheriyan2308@gmail.com>
2023-02-02 09:47:45 +0530

    path is modified

--- dhanasekar_progress/login_verify.js
+++ dhanasekar_progress/login_verify.js
@@ -14,13 +14,13 @@ function redirect() {
   if (mail_list == 1) {
     if (cred[mail_id] == cred[pass]) {
       window.location.href =
-        "C:/xampp/htdocs/lets_dream/Lets-dream-test-2062017/Lets-dream-Mentor-dashboard/index.html";
+        "../Lets-dream-Mentor-dashboard/index.html";
     } 
     
   } else {
     if (cred[mail_id] == cred[pass]) {
       window.location.href =
-        "C:/xampp/htdocs/lets_dream/Lets-dream-test-2062017/Lets-dream-Mentee-dashboard/index.html";
+        "../Lets-dream-Mentee-dashboard/index.html";
     }
   }
 }
