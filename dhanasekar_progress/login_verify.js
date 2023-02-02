90bed5c13f369223b72bd0017aa38381bb2ab856
Dhanasty <dhanasty@gmail.com>
2023-02-02 12:13:16 +0530

    Final login verify is updated

--- dhanasekar_progress/login_verify.js
+++ dhanasekar_progress/login_verify.js
@@ -2,24 +2,24 @@ const cred = {
   "qwerty_mentee@gmail.com": "qwerty",
   "qwerty_mentor@gmail.com": "qwerty",
 };
-const mail_id = document.getElementById("email-ef64").value;
-const pass = document.getElementById("password-ef64").value;
+const mail_id = document.querySelector("#email-ef64");
+const pass = document.querySelector("#password-ef64");
 const mail_list = {
   "qwerty_mentee@gmail.com": 2,
   "qwerty_mentor@gmail.com": 1,
 };
-const sub = document.getElementsById("sub");
+
 function redirect() {
   console.log("hello1");
-  if (mail_list == 1) {
+  if (mail_list[mail_id.value] == 1) {
     if (cred[mail_id] == cred[pass]) {
       window.location.href =
-        "C:/xampp/htdocs/lets_dream/Lets-dream-test-2062017/Lets-dream-Mentor-dashboard/index.html";
+        "../Lets-dream-Mentor-dashboard/index.html";
     }
-  } else {
+  } else if(mail_list[mail_id.value]==2) {
     if (cred[mail_id] == cred[pass]) {
       window.location.href =
-        "C:/xampp/htdocs/lets_dream/Lets-dream-test-2062017/Lets-dream-Mentee-dashboard/index.html";
+        "../Lets-dream-Mentee-dashboard/index.html";
     }
   }
 }
