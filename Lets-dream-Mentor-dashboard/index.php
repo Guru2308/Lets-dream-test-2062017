c80252970aad79455282b8a4b57f99790630791c
Guru2308 <chatheriyan2308@gmail.com>
2023-02-15 12:35:52 +0530

    updated chat box in mentor

--- Lets-dream-Mentor-dashboard/index.php
+++ Lets-dream-Mentor-dashboard/index.php
@@ -139,6 +139,8 @@
                 </tbody>
               </table>
 
+              
+
               <p class="u-text u-text-default u-text-1">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, accusamus.
               </p>
@@ -256,17 +258,46 @@
               </div>
             </div>
           </div>
+          <div id="center-text">
+    <!-- <h2>ChatBox UI</h2> -->
+    <p></p>
+    
+  </div> 
 
-          <div class="modal-footer">
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat float-left" viewBox="0 0 16 16">
-                <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
-              </svg>
-            </button>
-            <button type="button" class="btn btn-default" data-dismiss="modal">
-              Close
-            </button>
-          </div>
+<div id="body"> 
+  
+<div id="chat-circle" class="btn btn-raised">
+                          <div id="chat-overlay"></div>
+                          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
+                            <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+                          </svg>
+                    </div>
+  
+  <div class="chat-box">
+    <div class="chat-box-header">
+      Chat with mentee
+      <span class="chat-box-toggle"><i class="material-icons">X</i></span>
+    </div>
+    <div class="chat-box-body">
+      <div class="chat-box-overlay">   
+      </div>
+      <div class="chat-logs">
+       
+      </div><!--chat-log -->
+    </div>
+    <div class="chat-input">      
+      <form>
+        <input type="text" id="chat-input" placeholder="Send a message..."/>
+      <button type="submit" class="chat-submit" id="chat-submit"><i class="material-icons">Send</i></button>
+      </form>      
+    </div>
+  </div>
+  
+  
+  
+  
+</div>
+
         </div>
       </div>
     </div>
